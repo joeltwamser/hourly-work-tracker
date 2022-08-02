7321c39a0d4b620b4cce1bfa2c797fba73254ea2
Joel Wamser <joel.t.wamser@gmail.com>
2022-08-01 21:42:13 -0400

    Resizing of window from any edge or corner works, however the refresh rate/resize algorithm is fairly low/inefficient currently so I may work on that next.  Debating locking aspect ratio too, haven't decided.  Also made border thicker, and changed it from being an actual Border element/Control in XAML, to being the Rectangles that are used to handle the resizing of the application.
    Signed-off-by: Joel Wamser <joel.t.wamser@gmail.com>

--- hourlyWorkTracker/MainWindow.xaml.cs
+++ hourlyWorkTracker/MainWindow.xaml.cs
@@ -24,5 +24,73 @@ namespace hourlyWorkTracker
         {
             InitializeComponent();
         }
+
+        bool resize_window_in_process = false;
+        private void resizeWindowBegin(object sender, MouseButtonEventArgs e)
+        {
+            Rectangle? sender_rectangle = sender as Rectangle;
+            if (sender_rectangle != null)
+            {
+                resize_window_in_process = true;
+                sender_rectangle.CaptureMouse();
+            }
+        }
+
+        private void resizeWindowEnd(object sender, MouseButtonEventArgs e)
+        {
+            Rectangle? sender_rectangle = sender as Rectangle;
+            if (sender_rectangle != null)
+            {
+                resize_window_in_process = false;
+                sender_rectangle.ReleaseMouseCapture();
+            }
+        }
+
+        private void resizeWindow(object sender, MouseEventArgs e)
+        {
+            if(resize_window_in_process)
+            {
+                Rectangle? sender_rectangle = sender as Rectangle;
+                if(sender_rectangle != null)
+                {
+                    Window? main_window = sender_rectangle.Tag as Window;
+                    double width = e.GetPosition(main_window).X;
+                    double height = e.GetPosition(main_window).Y;
+                    sender_rectangle.CaptureMouse();
+                    if (sender_rectangle.Name.ToLower().Contains("right"))
+                    {
+                        width += 5;
+                        if (width > 0)
+                            main_window.Width = width;
+                    }
+                    if (sender_rectangle.Name.ToLower().Contains("left"))
+                    {
+                        width -= 5;
+                        main_window.Left += width;
+                        width = main_window.Width - width;
+                        if (width > 0)
+                        {
+                            main_window.Width = width;
+                        }
+                    }
+                    if (sender_rectangle.Name.ToLower().Contains("bottom"))
+                    {
+                        height += 5;
+                        if (height > 0)
+                            main_window.Height = height;
+                    }
+                    if (sender_rectangle.Name.ToLower().Contains("top"))
+                    {
+                        height -= 5;
+                        main_window.Top += height;
+                        height = main_window.Height - height;
+                        if (height > 0)
+                        {
+                            main_window.Height = height;
+                        }
+                    }
+                }
+            }
+        }
     }
 }
