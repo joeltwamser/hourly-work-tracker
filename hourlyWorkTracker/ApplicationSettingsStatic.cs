ae71e0ed7b17da46e1c49a614a7d9ea6fc3f91d6
Joel Wamser <joel.t.wamser@gmail.com>
2022-09-20 21:38:18 -0400

    I implemented a WHOLE bunch of features all together to make the fundamental core functionality of this app.  I have a start button and reset button that uses the user's inputted hourly wage to calculate how much money they make per as time goes by and 2 visible sections that show the user's money they've made for the current work session they are on, as well as their current total money they have ever made stored in this app.
    Currently there is an unhandled exception when you exit the app while the ticker is running.  All data is stored and brought from the backend file Settings.conf, and currently the ticker itself isn't visually customizable nor visually impressive.  Also you cannot yet view the logs of each of one's recorded sessions.

--- hourlyWorkTracker/ApplicationSettingsStatic.cs
+++ hourlyWorkTracker/ApplicationSettingsStatic.cs
@@ -6,8 +6,14 @@ using System.Threading.Tasks;
 
 namespace hourlyWorkTracker
 {
+    //Notes: This static class is used to store all of the main values that persist across the application between individual uses (closing and reopening the application for later use.)
+    //This may be bad practice as it feels like I'm using global memory.  I have also now set up events to be raised when certain values are changed from anywhere in my application.
+    //Again I feel like this may be bad practice since it feels like I'm using this class as one giant global variable but I'm not sure.
     internal static class ApplicationSettingsStatic
     {
+        //private fields
+        private static double _hourly_wage;
+        private static double _total_money;
         public static double OpacitySliderValue
         { get; set; }
 
@@ -15,9 +21,32 @@ namespace hourlyWorkTracker
         { get; set; }
 
         public static double HourlyWage
-        { get; set; }
+        {
+            get
+            {
+                return _hourly_wage;
+            }
+            set
+            {
+                _hourly_wage = value;
+                HourlyWageChanged?.Invoke(null, EventArgs.Empty);
+            }
+        }
 
         public static double TotalMoney
-        { get; set; }
+        {
+            get
+            {
+                return _total_money;
+            }
+            set
+            {
+                _total_money = value;
+                TotalMoneyChanged?.Invoke(null, EventArgs.Empty);
+            }
+        }
+
+        public static event EventHandler? HourlyWageChanged;
+        public static event EventHandler? TotalMoneyChanged;
     }
 }
--- hourlyWorkTracker/MainWindow.xaml.cs
+++ hourlyWorkTracker/MainWindow.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -12,6 +14,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Timers;
 
 namespace hourlyWorkTracker
 {
@@ -22,11 +25,67 @@ namespace hourlyWorkTracker
     {
         //fields
         private bool resize_window_in_process;
+        private const string _starting_money = "$0.00";
+        private System.Timers.Timer _timer;
+        private Stopwatch _stopwatch;
+        private bool _running = false;
         public MainWindow()
         {
             InitializeComponent();
             Opacity = ApplicationSettingsStatic.MainWindowOpacity;
+            ApplicationSettingsStatic.TotalMoneyChanged += onTotalMoneyChanged;
+            CurrentMoneyDisplay.Text = _starting_money;
+            TotalMoneyDisplay.Text = "$" + ApplicationSettingsStatic.TotalMoney.ToString("F2");
+
+            _stopwatch = new Stopwatch();
+            _timer = new System.Timers.Timer(10);
+            _timer.Elapsed += onTimerElapse;
         }
+
+        private void onTimerElapse(object? sender, ElapsedEventArgs e)
+        {
+            Application.Current.Dispatcher.Invoke(() =>
+            {
+                double money_made_this_session = _stopwatch.Elapsed.TotalHours * ApplicationSettingsStatic.HourlyWage;
+                CurrentMoneyDisplay.Text = "$" + money_made_this_session.ToString("F2");
+                TotalMoneyDisplay.Text = "$" + (ApplicationSettingsStatic.TotalMoney + money_made_this_session).ToString("F2");
+            });
+        }
+
+        private void onStartStopSessionClick(object sender, RoutedEventArgs e)
+        {
+            if (!_running)
+            {
+                _stopwatch.Start();
+                _timer.Start();
+                StartStopSessionButton.Content = "Stop";
+                StartStopSessionButton.FontWeight = FontWeights.Bold;
+                ResetSessionButton.IsEnabled = false;
+                _running = true;
+            }
+            else
+            {
+                _stopwatch.Stop();
+                _timer.Stop();
+                StartStopSessionButton.Content = "Start";
+                StartStopSessionButton.FontWeight = FontWeights.Bold;
+                ResetSessionButton.IsEnabled = true;
+                _running = false;
+            }
+        }
+
+        private void onResetSessionClick(object sender, RoutedEventArgs e)
+        {
+            _stopwatch.Reset();
+            CurrentMoneyDisplay.Text = _starting_money;
+            saveTotalMoney();
+        }
+
+        private void onTotalMoneyChanged(object? sender, EventArgs e)
+        {
+            TotalMoneyDisplay.Text = "$" + ApplicationSettingsStatic.TotalMoney.ToString("F2");
+        }
+
         private void resizeWindowBegin(object sender, MouseButtonEventArgs e)
         {
             if (e.ChangedButton != MouseButton.Left)
@@ -123,8 +182,30 @@ namespace hourlyWorkTracker
 
         private void openConfigureWindow(object sender, RoutedEventArgs e)
         {
-            ConfigureWindow my_configure_window = new ConfigureWindow();
-            my_configure_window.ShowDialog();
+            if (_running)
+            {
+                MessageBox.Show("Please pause the timer before configuration.");
+            }
+            else
+            {
+                ConfigureWindow my_configure_window = new ConfigureWindow();
+                my_configure_window.ShowDialog();
+            }
+        }
+
+        private void onClosed(object sender, EventArgs e)
+        {
+            saveTotalMoney();
+        }
+
+        private void saveTotalMoney()
+        {
+            double temp;
+            bool success = double.TryParse(TotalMoneyDisplay.Text.Trim('$'), out temp);
+            if (success)
+            {
+                ApplicationSettingsStatic.TotalMoney = temp;
+            }
         }
     }
 }
